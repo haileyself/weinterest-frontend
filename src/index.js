e02e634a457b1dc4a20f7e8554dccdfaeb510e24
jingyong9111 <jingyong9111@gmail.com>
2019-09-16 20:50:09 +0900

    초기세팅완료

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Styles/Index.scss";
+import "./Styles/Reset.scss";
+import Router from "./Router";
+
+ReactDOM.render(<Router />, document.getElementById("root"));

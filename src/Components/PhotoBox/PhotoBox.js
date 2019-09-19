99e4137b9a7dc0e040922397aac2a6531589a9e4
alice4xx <aliceaprilx@gmail.com>
2019-09-19 11:50:00 +0900

    wip

---
+++ src/Components/PhotoBox/PhotoBox.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+//import { Link } from "react-router-dom";
+import "./PhotoBox.scss";
+
+class PhotoBox extends Component {
+  render() {
+    return (
+      <div className="photoCompo">
+        <figure>
+          <div className="xoxoxo">
+            <div className="pinButton">
+              <i class="fas fa-thumbtack x"></i>
+              <span>저장</span>
+            </div>
+            <a href="/">
+              <img src="https://i.pinimg.com/564x/d7/68/6b/d7686bcbf91f7fd4eaa96c35b47fd10f.jpg" />
+              <div className="xoxo" />
+            </a>
+          </div>
+          <p>추천 핀</p>
+        </figure>
+      </div>
+    );
+  }
+}
+
+export default PhotoBox;

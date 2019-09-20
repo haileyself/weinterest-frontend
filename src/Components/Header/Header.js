923048860be8117e4bb19dafc19509165b0ddc9b
alice4xx <aliceaprilx@gmail.com>
2019-09-20 17:08:06 +0900

    mapmap입니다

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
 import Logo from "Images/Logo.png";
 import "./Header.scss";
+import HeaderCompo from "./HeaderCompo";
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = { searchEngine: "unclick" };
+  }
+
+  showMeList = e => {
+    this.setState({ searchEngine: "click" });
+  };
+
   render() {
     return (
       <div className="headerContainer">
@@ -14,21 +25,35 @@ class Header extends Component {
               </div>
             </a>
           </div>
-          <div className="headerSearch">
+          <div
+            className={`headerSearch ${
+              this.state.searchEngine === "click"
+                ? "listToSearch "
+                : "headerSearch"
+            }`}
+          >
             <i className="fas fa-search small_search_Icon" />
-            <input type="email" className="searchsearch" placeholder="검색" />
+            <input
+              type="email"
+              className="searchsearch"
+              placeholder="검색"
+              onClick={this.showMeList}
+            />
+            <HeaderCompo />
           </div>
           <div className="theOthers">
-            <div className="home_1">홈</div>
+            <a href="/">
+              <div className="home_1">홈</div>
+            </a>
             <div className="home_2">팔로잉</div>
             <div className="home_3">alice</div>
           </div>
           <div className="theRestOf">
             <div className="box"></div>
             <div className="restWrap">
-              <i class="fas fa-comment-dots msg"></i>
-              <i class="fas fa-bell msg"></i>
-              <i class="fas fa-ellipsis-h msg"></i>
+              <i className="fas fa-comment-dots msg"></i>
+              <i className="fas fa-bell msg"></i>
+              <i className="fas fa-ellipsis-h msg"></i>
             </div>
           </div>
         </div>

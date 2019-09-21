6832ee4c1b7081363b266f66ae511a658cc7d879
jingyong9111 <jingyong9111@gmail.com>
2019-09-21 10:22:40 +0900

    로그인페이지 기능구현 [{페치},{값 없을때 sass}]

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,23 +1,49 @@
 import React from "react";
-import "./Login.scss";
 import logo from "../../Images/Logo.png";
+import "./Login.scss";
 
 class Login extends React.Component {
   constructor() {
     super();
-    this.state = { valueId: "" };
+    this.state = { valueId: "", valuePw: "" };
   }
+  valueCheck = () => {
+    if (this.state.valueId === "") {
+      this.setState({ valueId: "change" });
+    }
+  };
   inputValueId = e => {
     this.setState({ valueId: e.target.value });
   };
-  move = () => {
+  signupMove = () => {
     this.props.history.push("signup");
+  };
+  onClickLogin = () => {
+    // fetch("http://localhost:8000/users/login", {
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     body: JSON.stringfy({
+    //       email: this.state.valueId,
+    //       password: this.state.valuePw
+    //     })
+    //   }
+    // })
+    //   .then(response => response.json())
+    //   .then(response => {
+    //     if (response.access_token) {
+    //       localStorage.setItem("이건정하자", response.access_token);
+    //       this.props.history.push("/");
+    //     }
+    //   });
+    if (this.state.valueId === "") {
+      this.setState({ valueId: "change" });
+    }
   };
   render() {
     return (
       <div>
         <div className="login_wrap">
-          <div onClick={this.move} className="login_button">
+          <div onClick={this.signupMove} className="login_button">
             회원가입
           </div>
           <div className="login_box_wrap">
@@ -29,13 +55,23 @@ class Login extends React.Component {
               </div>
               <div className="Login_title">Welcome to pinterest</div>
               <input
-                onChang={this.inputValueId}
-                className="Login_input"
+                onChange={this.inputValueId}
+                className={`Login_input ${
+                  this.state.valueId === "change" ? "input_change" : ""
+                }`}
                 placeholder="이메일"
               ></input>
+              {this.state.valueId === "change" && (
+                <div className="different">
+                  놓친 부분이 있네요! 이메일을 추가하세요
+                </div>
+              )}
+              <div className="red_border"></div>
               <input className="Login_input2" placeholder="비밀번호"></input>
-              <div className="login_forget">비밀번호를 잃잊으셨나요?</div>
-              <div className="login_button">로그인</div>
+              <div className="login_forget">비밀번호를 잃으셨나요?</div>
+              <div onClick={this.onClickLogin} className="login_button">
+                로그인
+              </div>
               <div className="login_or">또는</div>
               <div className="login_fB"></div>
               <div className="login_Gg"></div>
